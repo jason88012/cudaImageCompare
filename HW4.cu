3314dd30d92db20925c4eda04a6dfb84ce45f161
jason88012 <jason40175032h@gmail.com>
2017-05-17 16:11:30 +0800

    Update HW4.cu

--- HW4.cu
+++ HW4.cu
@@ -1,241 +1,207 @@
-#include "book.h"
+#include "opencv/cv.h"
+#include "opencv/highgui.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 #include <time.h>
 #include <cuda_runtime.h>
+#define VALUE_MAX 10000000
+#define MAX_THREAD 1024
 
-// A(m*n) * B(n*l)
-const int m = 1024;
-const int n = 1024;
-const int l = 1024;
-const int TILE_WIDTH = 32;
+struct match{
+	int bestRow;
+	int bestCol;
+	int bestSAD;
+}position;
 
-const int sizeA = m*n*sizeof(int);
-const int sizeB = n*l*sizeof(int);
-const int sizeC = m*l*sizeof(int);
-
-dim3 blocksPerGrid(m/TILE_WIDTH, l/TILE_WIDTH);
-dim3 threadsPerBlock(TILE_WIDTH, TILE_WIDTH);
-
-//dim3 blocksPerGrid((l+threadsPerBlock.x-1)/threadsPerBlock.x, 
-//					(m+threadsPerBlock.y-1)/threadsPerBlock.y );
-
-// define function
-void PrintArray(int *A, int l);
-void PrintMatrix(int *M, int row, int col);
-int *MallocMatrix(int row, int col, int attr);
-int *Transport(int* B, int row, int col);
-int *MetrixMult(int *A, int *B);
-
-__global__ void CudaMetrixMult(int *A, int *B, int *C)
+__global__ void cudaTempMatch(unsigned char *sourcePixel, unsigned char *patternPixel, int *result,
+							  int result_w, int src_w, int pat_w)
 {
-	int blockRow = blockIdx.y;
-	int blockCol = blockIdx.x;
-	int threadRow = threadIdx.y;
-	int threadCol = threadIdx.x;
+	//int blockCol = blockIdx.x;
+	//int blockRow = blockIdx.y;
+	int tidx = threadIdx.x;
+	int tidy = threadIdx.y;
+	int tidSource = 512*(threadIdx.y + blockIdx.y) + threadIdx.x + blockIdx.x;
+	int tidPattern = 48*threadIdx.y + threadIdx.x;
+	int SAD = 0;
+	//unsigned char subSrcElement;
+	//unsigned char patElement;
+	//unsigned char *subSource = sourcePixel + src_w*blockRow + blockCol;
+	__shared__ unsigned char patternCache[48*48];
+	__shared__ unsigned char subSrcElement[48*48];
 
-	__shared__ int subA_element[TILE_WIDTH][TILE_WIDTH];
-	__shared__ int subB_element[TILE_WIDTH][TILE_WIDTH];
-
-	int *subC = C + blockRow*m*TILE_WIDTH + blockCol*TILE_WIDTH;
-	int sum = 0;
-
-	for (int i = 0; i < n/TILE_WIDTH; ++i)
+	for (int i = 0; i < 2; ++i)
 	{
-		int *subA = A + blockRow*m*TILE_WIDTH + i*TILE_WIDTH;
-		int *subB = B + i*m*TILE_WIDTH + blockCol*TILE_WIDTH;
-		subA_element[threadRow][threadCol] = *(subA + threadRow*n + threadCol);
-		subB_element[threadRow][threadCol] = *(subB + threadRow*n + threadCol);
-		__syncthreads();
-
-		for (int j = 0; j < TILE_WIDTH; ++j)
+		for (int j = 0; j < 2; ++j)
 		{
-
-			//int Aelement = *(subA + threadRow*n + j);
-			//int Belement = *(subB + j*n + threadCol);
-			//sum += Aelement*Belement;
-			sum += subA_element[threadRow][j]*subB_element[j][threadCol];
+			//subSrcElement = *(sourcePixel + i*512*24 + j*24 + tidSource);
+			subSrcElement[24*48*i + 24*j + 48*tidy + tidx] = *(sourcePixel + i*512*24 + j*24 + tidSource);
+			patternCache[24*48*i + 24*j + 48*tidy + tidx] = *(patternPixel + i*48*24 + j*24 + tidPattern);
+			SAD += abs(subSrcElement[24*48*i + 24*j + 48*tidy + tidx] - 
+					   patternCache[24*48*i + 24*j + 48*tidy + tidx]);			
 		}
-		__syncthreads();
 	}
-	*(subC + threadRow*n + threadCol) = sum;
+	//__syncthreads();
+
+	*(result + blockIdx.y*result_w + blockIdx.x) = SAD;
 }
 
-int main(void)
+int main( int argc, char** argv )
 {
-	int *A, *B, *BT, *C, *CP;
-	int *A_cuda, *B_cuda, *C_cuda;
-	int i,check;
-	double diff;
+	IplImage* sourceImg; 
+	IplImage* patternImg; 
+	CvPoint pt1, pt2;
+	int minSAD = VALUE_MAX;
+	int x, y;
 	struct timespec t_start, t_end;
+	double elapsedTime;
+	int result_height;
+	int result_width;
+	unsigned char *sourcePixelData, *patternPixelData;
+	unsigned char *source_d, *pattern_d;
+	int *result_d;
+	int *host_result;
 
-	// 1 = random matrix, 0 = all zero matrix, -1 = only allocate memory
-	A = MallocMatrix(m, n, 1);
-	B = MallocMatrix(n, l, 1);
-	CP = MallocMatrix(m, l, 0);
-
-	// allocate device memory
-	HANDLE_ERROR(cudaMalloc((void**)&A_cuda, sizeA));
-	HANDLE_ERROR(cudaMalloc((void**)&B_cuda, sizeB));
-	HANDLE_ERROR(cudaMalloc((void**)&C_cuda, sizeC));
-
-	// multiply in CPU============================================================
-	clock_gettime( CLOCK_REALTIME, &t_start);
-	printf("Metrix multiply in sequential:\n");
-
-	//C = MetrixMult(A, B);
-	//PrintMatrix(C, m, l);
-
-	clock_gettime( CLOCK_REALTIME, &t_end);
-	diff = (t_end.tv_sec - t_start.tv_sec) * 1000.0;   
-	diff += (t_end.tv_nsec - t_start.tv_nsec) / 1000000.0;   
-	printf("matrix mult in sequential elapsedTime: %lf ms\n", diff);
-
-	// multiply in cuda shared memory==============================================
-	clock_gettime( CLOCK_REALTIME, &t_start);
-	printf("Metrix multiply with CUDA:\n");
-
-	// copy A1 and B1 datas to device
-	HANDLE_ERROR(cudaMemcpy(A_cuda, A, sizeA,
-							cudaMemcpyHostToDevice));
-	HANDLE_ERROR(cudaMemcpy(B_cuda, B, sizeB,
-							cudaMemcpyHostToDevice)); 		 
-
-	// kernal function
-	CudaMetrixMult<<<blocksPerGrid, threadsPerBlock>>>(A_cuda, B_cuda, C_cuda);
-
-	// copy datas back to host from device
-	HANDLE_ERROR(cudaMemcpy(CP, C_cuda, sizeC,
-							cudaMemcpyDeviceToHost)); 
-
-	clock_gettime( CLOCK_REALTIME, &t_end);
-	diff = (t_end.tv_sec - t_start.tv_sec) * 1000.0;   
-	diff += (t_end.tv_nsec - t_start.tv_nsec) / 1000000.0;   
-	printf("matrix mult with CUDA elapsedTime: %lf ms\n", diff);
-
-	// free device's memory
-	cudaFree(A_cuda);  cudaFree(B_cuda);  cudaFree(C_cuda);
-
-	//PrintMatrix(CP, m, l);
-	
-	/*
-	check = 0;
-	for (i = 0; i < m*l; ++i)
+	if( argc != 3 )
 	{
-		if (C[i] != CP[i])
+		printf("Using command: %s source_image search_image\n",argv[0]);
+		exit(1);
+	}
+
+	// Load source image
+	if((sourceImg = cvLoadImage( argv[1], 0)) == NULL)
+	{
+		printf("%s cannot be openned\n",argv[1]);
+		exit(1);
+	}
+
+	const int sizeSource = sourceImg->imageSize * sizeof(unsigned char);
+	printf("height of sourceImg:%d\n",sourceImg->height);
+	printf("width of sourceImg:%d\n",sourceImg->width);
+	printf("size of sourceImg:%d\n",sourceImg->imageSize); 
+
+	// Load pattern image
+	if((patternImg = cvLoadImage( argv[2], 0)) == NULL)
+	{
+		printf("%s cannot be openned\n",argv[2]);
+		exit(1);
+	}
+
+	const int sizePattern = patternImg->imageSize * sizeof(unsigned char);
+	printf("height of sourceImg:%d\n",patternImg->height);
+	printf("width of sourceImg:%d\n",patternImg->width);
+	printf("size of sourceImg:%d\n",patternImg->imageSize);
+
+	// Define result size
+	result_height = sourceImg->height - patternImg->height + 1;
+	result_width = sourceImg->width - patternImg->width + 1;
+	const int sizeResult = result_height * result_width * sizeof(int);
+	host_result=(int*)malloc(sizeResult);
+
+	// Record Image pixel datas in CPU
+	sourcePixelData = (unsigned char*)malloc(sizeSource);
+	patternPixelData = (unsigned char*)malloc(sizePattern);
+
+	for (int i = 0; i < sourceImg->height; ++i)
+	{
+		for (int j = 0; j < sourceImg->width; ++j)
 		{
-			printf("err = CP[%d]\t", i);
-			printf("correct = %d\t", C[i]);
-			printf("err = %d\n", CP[i]);
-			check = 1;
-			//break;
+			sourcePixelData[i*sourceImg->widthStep + j] = sourceImg->imageData[i*sourceImg->widthStep + j];
 		}
 	}
 
-	
-	if (check == 1)
+	for (int i = 0; i < patternImg->height; ++i)
 	{
-		printf("Fail!!!\n");
+		for (int j = 0; j < patternImg->width; ++j)
+		{
+			patternPixelData[i*patternImg->widthStep + j] = patternImg->imageData[i*patternImg->widthStep + j];
+		}
 	}
-	else
+
+	// start time
+	clock_gettime( CLOCK_REALTIME, &t_start);
+
+	// allocate device memory
+	cudaMalloc((void**)&source_d, sizeSource);
+	cudaMalloc((void**)&pattern_d, sizePattern);
+	cudaMalloc((void**)&result_d, sizeResult);
+
+	// copy datas to device from host
+	cudaMemcpy(source_d, sourcePixelData, sizeSource,
+							cudaMemcpyHostToDevice);
+	cudaMemcpy(pattern_d, patternPixelData, sizePattern,
+							cudaMemcpyHostToDevice);
+
+	// kernel function
+	dim3 blocksPerGrid(result_width, result_width);
+	dim3 threadsPerBlock(24, 24);
+	cudaTempMatch<<<blocksPerGrid, threadsPerBlock>>>
+	(source_d, pattern_d, result_d, result_width, sourceImg->width, patternImg->width);
+
+	// copy results to host from device
+	cudaMemcpy(host_result, result_d, sizeResult,
+							cudaMemcpyDeviceToHost); 
+
+	// stop time
+	clock_gettime( CLOCK_REALTIME, &t_end);
+	// compute and print the elapsed time in millisec
+	elapsedTime = (t_end.tv_sec - t_start.tv_sec) * 1000.0;
+	elapsedTime += (t_end.tv_nsec - t_start.tv_nsec) / 1000000.0;
+	printf("%lf ms\n", elapsedTime);
+
+	/*
+	int count = 0;
+	for (int i = 0; i < sizeResult/sizeof(int); ++i)
 	{
-		printf("Sucess!!!\n");
+		printf("%d ", host_result[i]);
+		count++;
+		if(count%10 == 0)
+		{
+			printf("\n");
+		}
 	}
 	*/
 
+	// find min SAD's sub source matrix place
+	for( y=0; y<result_height; y++ ) 
+	{
+		for( x=0; x<result_width; x++ ) 
+		{
+			if ( minSAD > host_result[y * result_width + x] )
+			{
+				minSAD = host_result[y * result_width + x];
+
+				// give me VALUE_MAX
+				position.bestRow = y;
+				position.bestCol = x;
+				position.bestSAD = host_result[y * result_width + x];
+			}
+		}
+	}
+
+	printf("minSAD is %d\n", minSAD);
+
+	//setup the two points for the best match
+	pt1.x = position.bestCol;
+	pt1.y = position.bestRow;
+	pt2.x = pt1.x + patternImg->width;
+	pt2.y = pt1.y + patternImg->height;
+
+	printf("the point is (%d, %d) and (%d, %d)\n", pt1.x, pt1.y, pt2.x, pt2.y);
+
+	// Draw the rectangle in the source image
+	cvRectangle( sourceImg, pt1, pt2, CV_RGB(0,255,0), 3, 8, 0 );
+	cvNamedWindow( "sourceImage", 1 );
+	cvShowImage( "sourceImage", sourceImg );
+	cvNamedWindow( "patternImage", 1 );
+	cvShowImage( "patternImage", patternImg );
+	cvWaitKey(0); 
+
+	cvDestroyWindow( "sourceImage" );
+	cvReleaseImage( &sourceImg );
+	cvDestroyWindow( "patternImage" );
+	cvReleaseImage( &patternImg );
+
 	return 0;
-}
-
-void PrintArray(int *A, int l)
-{
-	int i;
-	for (i = 0; i < l; ++i)
-	{
-		printf("%d\t", A[i]);
-	}
-}
-
-void PrintMatrix(int *M, int row, int col)
-{
-	int i,j;
-	for (i = 0; i < row; ++i)
-	{
-		for (j = 0; j < col; ++j)
-		{
-			printf("%d\t", M[i*col + j]);
-		}
-		printf("\n");
-	}
-	printf("\n");
-}
-
-// attr = 1:random; 0:all zero; -1:only allocate memory;
-int* MallocMatrix(int row, int col, int attr)
-{
-	int *A;
-	int i,j;
-
-	A = (int*)malloc( row*col*sizeof(int) );
-
-	if (attr == 1)
-	{
-		for (i = 0; i < row; ++i)
-		{
-			for (j = 0; j < col; ++j)
-			{
-				A[i*col + j] = rand()%10;
-				//A[i*col + j] = 1;
-			}
-		}
-	}
-	else if (attr == 0)
-	{
-		for (i = 0; i < row; ++i)
-		{
-			for (j = 0; j < col; ++j)
-			{
-				A[i*col + j] = 0;
-			}
-		}
-	}
-	return A;
-}
-
-// matrix transport
-int *Transport(int* B, int row, int col)
-{
-	int *BT;
-	int i,j;
-
-	BT = MallocMatrix(col, row, -1);
-
-	for (i = 0; i < row; ++i)
-	{
-		for (j = 0; j < col; ++j)
-		{
-			BT[j*row + i] = B[i*col + j];
-		}
-	}
-	return BT;
-}
-
-int* MetrixMult(int *A, int *B)
-{
-	int *C;
-	int i,j,k;
-
-	C = MallocMatrix(m, l, 0);
-
-	for (i = 0; i < m; ++i)
-	{
-		for (j = 0; j < l; ++j)
-		{
-			for (k = 0; k < n; ++k)
-			{
-				C[i*l + j] += A[i*m + k] * B[k*l + j];
-			}
-		}
-	}
-	return C;
 }
 
